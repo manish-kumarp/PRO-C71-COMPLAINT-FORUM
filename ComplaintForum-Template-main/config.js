c1d655754f92e6c338a92eae5272887b9b6a9e5c
manish-kumarp <75120291+manish-kumarp@users.noreply.github.com>
2021-09-01 17:34:51 +0530

    Add files via upload

---
+++ ComplaintForum-Template-main/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDdzdJeRiRJy4AAO9RZeSKnu_NQ6hJCb6I",
+  authDomain: "complaint-forum-project.firebaseapp.com",
+  projectId: "complaint-forum-project",
+  storageBucket: "complaint-forum-project.appspot.com",
+  messagingSenderId: "886497714919",
+  appId: "1:886497714919:web:32f1bceaa382fd8b383842"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default fImageBackgroundBase.firestore()
+
